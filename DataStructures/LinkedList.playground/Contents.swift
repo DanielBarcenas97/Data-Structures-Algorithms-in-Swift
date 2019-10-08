0cdb463921861bc9f546013b43320a205b4b671f
DanielBarcenas97 <barcenas.proteco@gmail.com>
2019-10-08 11:46:44 -0500

    Linked List playground

---
+++ DataStructures/LinkedList.playground/Contents.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+public class Node <Value> {
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil){
+        self.value = value
+        self.next = next
+        
+    }
+}
+
+
+extension Node:CustomStringConvertible{
+    public var description: String{
+        guard let next  = next else{
+            return "\(value)"
+        }
+        return "\(value)-> " + String(describing: next) + " "
+    }
+}
+
+
+public class LinkedList <Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init(){}
+    
+    public var isEmpty:Bool {
+        return head == nil
+    }
+    
+}
+
+extension LinkedList:CustomStringConvertible{
+    public var description: String{
+        guard let head  = head else{
+            return "empty List"
+        }
+        return String(describing: head) + " "
+    }
+    
+    public func push(_ value: Value){
+        head = Node(value: value, next : head)
+        if tail == nil{
+            tail = head
+        }
+        
+    }
+}
+
+
+
+print("The list ")
+/*
+ let node1 = Node(value:5)
+ let node2 = Node(value:6)
+ let node3 = Node(value:7)
+ 
+ node1.next=node2
+ node2.next=node3
+ */
+var list = LinkedList<Int>()
+
+print(list)  //List is empty
+
+list.push(5) //Add element in Head
+list.push(6)
+list.push(7)
+
+print(list)
